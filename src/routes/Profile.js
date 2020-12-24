269052c690c0498225defc3099b163be9cde9972
HwaGyeong <dlghkrud18@naver.com>
2020-12-24 20:02:43 +0900

    nweeting

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,2 +1,18 @@
+import { authService } from "fbase";
 import React from "react";
-export default () => <span>Profile</span>;
+import { useHistory } from "react-router-dom";
+
+
+export default () => {
+    const history=useHistory();
+    const onLogOutClick =()=> {
+        authService.signOut();
+        history.push("/");
+    };
+
+return (
+  <>
+    <button onClick={onLogOutClick} >Log Out</button>
+  </>  
+);
+};
